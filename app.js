caa5cb28cf4fba0d34647ac9d17b1b34318a8fbb
Denys Stroiu <denys.stroiu@gmail.com>
2021-08-09 16:28:38 +0300

    add app js file

---
+++ app.js
@@ -0,0 +1,53 @@
+// 1 peremenaia
+const firstName = 'David'
+const lastName = 'Bard'
+const isProgrammer = true
+//const age = 30
+
+const _ = 'private'
+const $ = 'some value'
+
+// const if = 'mkef' // err
+const withNum5 = '5'
+// const 5withNum = 5 // err
+
+// 2 Mutirovanie
+// console.log('imai cheloveka: ' + firstName + ', vozrast cheloveka: ' + age)
+// alert('Imai cheloveka: ' + firstName + ', vozrast cheloveka: ' + age)
+// const lastName = prompt('vvedite FIO')
+// alert(firstName + ' ' + lastName)
+
+// 3 operatoru
+let currentYear = 2020
+const birthYear = 1990 
+// const age = currentYear - birthYear
+
+const a = 10
+const b = 5
+
+let c = 32
+// c = c + a
+// console.log(c) 
+// c = c - a
+// console.log(c) 
+// c = c * a
+// console.log(c) 
+// c = c / a
+// console.log(c) 
+// c += a
+// console.log(c) 
+// c -= a
+// console.log(c) 
+// c *= a
+// console.log(c) 
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(++currentYear)
+// console.log(--currentYear)
+console.log(c)
+
+// console.log(age)
